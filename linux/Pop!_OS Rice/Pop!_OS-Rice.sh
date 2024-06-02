c2379f8907d55d46cb206874bee0260c7cc27087
Joshua Ross <josh@rossjm.dev>
2024-06-02 12:00:48 -0400

    modified rice script.

--- linux/Pop!_OS Rice/Pop!_OS-Rice.sh
+++ linux/Pop!_OS Rice/Pop!_OS-Rice.sh
@@ -3,6 +3,17 @@
 # Github: https://github.com/ColoredBytes
 # Purpose: Pop!_OS Rice Script
 
+update() {
+    sudo apt update
+    sudo apt upgrade -y
+}
+
+# Update The System
+update
+
+# Install Gnome Tweaks
+sudo apt install gome-tweaks
+
 # Copy Over themes
 cp -r Dracula /usr/share/themes
 cp -r Sweet-Dark-v40 /usr/share/themes
