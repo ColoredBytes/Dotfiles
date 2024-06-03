195259725e01a7112873ca8993e1d1e25add45bb
Joshua Ross <josh@rossjm.dev>
2024-06-03 10:35:12 -0400

    added more aliases to the git config script.

--- windows/Git/git_config.ps1
+++ windows/Git/git_config.ps1
@@ -11,8 +11,13 @@ git config --global user.name "$git_user"
 git config --global user.email "$git_email"
 
 # Set Git Aliases
-git config --global alias.ac "commit -am"
 git config --global alias.cl "clone"
+git config --global alias.pl "pull"
+git config --global alias.ph "push"
+git config --global alias.s "status"
+git config --global alias.ac "commit -am"
+git config --global alias.co "checkout"
+git config --global alias.cob "checkout -b"
 
 # Set Git Core
 git config --global core.eol lf
