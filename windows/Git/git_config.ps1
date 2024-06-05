692d088912ea271c3b3c89a4d7f44948f4c0c02a
Joshua Ross <josh@rossjm.dev>
2024-06-05 09:55:01 -0400

    Update git_config.ps1

--- windows/Git/git_config.ps1
+++ windows/Git/git_config.ps1
@@ -6,10 +6,10 @@ $git_user = Read-Host "Enter your Name"
 $git_email = Read-Host "Enter your Email"
 
 
-
 # Set Git user configuration
 git config --global user.name "$git_user"
 git config --global user.email "$git_email"
+
 
 # Set Git Aliases
 git config --global alias.cl "clone"
@@ -20,8 +20,9 @@ git config --global alias.ac "commit -am"
 git config --global alias.co "checkout"
 git config --global alias.cob "checkout -b"
 
+
 # Set Git Core
 git config --global core.eol lf
 git config --global core.autocrlf input
 
-Write-Output "Git configuration has been updated."
+Write-Information ".gitconfig has been Created..."
