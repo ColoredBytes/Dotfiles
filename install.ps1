9f17fa54b2b7d2c8bd3ad307c10ccc6c273dcf39
Joshua Ross <josh@rossjm.dev>
2024-06-05 20:26:37 -0400

    added install scripts for dotfiles

---
+++ install.ps1
@@ -0,0 +1,14 @@
+# Author: Joshua Ross
+# Github: https://github.com/ColoredBytes
+# Purpose: ms dotfiles setup script
+
+# Configure Git
+powershell.exe -ExecutionPolicy Bypass -File .\git\git_config.ps1
+
+# Setup oh-my-posh
+powershell.exe -ExecutionPolicy Bypass -File .\ms\posh-install.ps1
+
+# Install neovim
+powershell.exe -ExecutionPolicy Bypass -File .\ms\neovim-install.ps1
+
+
---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Author: Joshua Ross
+# Github: https://github.com/ColoredBytes
+# Purpose: nix dotfiles setup script
+
+# Configure Git
+
+
+# Setup oh-my-posh
+
+
+# Install neovim
+
+
+
