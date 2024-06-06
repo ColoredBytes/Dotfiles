bb803e849ed926e50be124d39c0a0ee8e496dcb1
Joshua Ross <josh@rossjm.dev>
2024-06-05 20:48:24 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -3,7 +3,7 @@
 # Purpose: ms dotfiles setup script
 
 # Configure Git
-powershell.exe -ExecutionPolicy Bypass -File .\git\git_config.ps1
+powershell.exe -ExecutionPolicy Bypass -File .\git\git-config.ps1
 
 # Setup oh-my-posh
 powershell.exe -ExecutionPolicy Bypass -File .\ms\posh-install.ps1
