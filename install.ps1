d68a38cbf1d334268e2f8ff868c500f98b877f6b
Joshua Ross <josh@rossjm.dev>
2024-06-05 21:00:13 -0400

    double quoted paths in install.ps1 script

--- install.ps1
+++ install.ps1
@@ -3,12 +3,12 @@
 # Purpose: ms dotfiles setup script
 
 # Configure Git
-powershell.exe -ExecutionPolicy Bypass -File .\git\git-config.ps1
+powershell.exe -ExecutionPolicy Bypass -File ".\git\git-config.ps1"
 
 # Setup oh-my-posh
-powershell.exe -ExecutionPolicy Bypass -File .\ms\posh-install.ps1
+powershell.exe -ExecutionPolicy Bypass -File ".\ms\posh-install.ps1"
 
 # Install neovim
-powershell.exe -ExecutionPolicy Bypass -File .\ms\neovim-install.ps1
+powershell.exe -ExecutionPolicy Bypass -File ".\ms\neovim-install.ps1"
 
 
