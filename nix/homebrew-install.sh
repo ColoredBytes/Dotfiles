c069c681c0940f8295a3f9d427046074b69bc677
Joshua Ross <josh@rossjm.dev>
2024-06-06 20:34:56 -0400

    Update homebrew-install.sh

--- nix/homebrew-install.sh
+++ nix/homebrew-install.sh
@@ -10,11 +10,7 @@
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
 # Install Homebrew's dependencies if you have sudo access:
-<<<<<<< HEAD
 sudo apt install build-essential -y
-=======
-sudo apt install build-essential
->>>>>>> 0cdf2969b6e8b401857a7b736acd48a7bc7524f2
 
 # update .bashrc
 exec bash
