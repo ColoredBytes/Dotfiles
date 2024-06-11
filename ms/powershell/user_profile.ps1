568a0ff761948c1996135b139980a29ae9b7699e
Joshua Ross <josh@rossjm.dev>
2024-06-10 21:39:35 -0400

    added takuya.omp.json theme also took inspo from him and created a central script that is just called

---
+++ ms/powershell/user_profile.ps1
@@ -0,0 +1,26 @@
+# set PowerShell to UTF-8
+[console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
+
+# Env
+$omp_config = Join-Path $PSScriptRoot ".\takuya.omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -PredictionSource History
+
+# Alias
+Set-Alias -Name vim -Value nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+# zoxide variable
+Invoke-Expression (& { (zoxide init powershell | Out-String) }) 
