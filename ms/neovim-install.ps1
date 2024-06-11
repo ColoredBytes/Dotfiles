1dd07a8755080ff9fa5dc71d7d24c1995d82d0ee
Joshua Ross <josh@rossjm.dev>
2024-06-10 20:39:51 -0400

    changed variables in the script to make it easier for profile

--- ms/neovim-install.ps1
+++ ms/neovim-install.ps1
@@ -3,7 +3,25 @@
 
 # Variables
 $REPO = "https://github.com/ColoredBytes/NvChad-v2-config.git"
-$INSTALL_PATH = "$env:LOCALAPPDATA\nvim"
+$INSTALL_PATH = "$env:USERPROFILE\.config\"
+$PROFILEPATH = $PROFILE.CurrentUserAllHosts
+$SETUPCOMMANDS = @' 
+# Set up XDG directories and environment variables
+if (-not (Test-Path "${ENV:USERPROFILE}\.config")) {
+    $null = mkdir "${ENV:USERPROFILE}\.config"
+}
+$env:XDG_CONFIG_HOME = "${ENV:USERPROFILE}\.config"
+
+if (-not (Test-Path "${ENV:USERPROFILE}\.local\share")) {
+    $null = mkdir "${ENV:USERPROFILE}\.local\share"
+}
+$env:XDG_DATA_HOME = "${ENV:USERPROFILE}\.local\share"
+
+if (-not (Test-Path "${ENV:USERPROFILE}\.local\state")) {
+    $null = mkdir "${ENV:USERPROFILE}\.local\state"
+}
+$env:XDG_STATE_HOME = "${ENV:USERPROFILE}\.local\state"
+'@
 
 # Install dependencies from JSON file
 winget import --import-file ".\assets\neovimreq.json"
@@ -14,16 +32,17 @@ if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
     exit 1
 }
 
-# Clone Git repository
-if (Test-Path $INSTALL_PATH) {
-    Write-Output "Removing existing installation at $INSTALL_PATH"
-    Remove-Item -Recurse -Force $INSTALL_PATH
-}
-
-git clone $REPO $INSTALL_PATH
+# Clone git repo
+git clone $REPO "$INSTALL_PATH\nvim"
 
 if ($?) {
     Write-Output "Neovim configuration cloned successfully to $INSTALL_PATH"
 } else {
     Write-Error "Failed to clone the repository."
 }
+
+# Append the setup commands to the profile script
+Add-Content -Path $PROFILEPATH -Value $SETUPCOMMANDS
+
+# Load the new profile
+. $profilePath
--- ms/posh-install.ps1
+++ ms/posh-install.ps1
@@ -3,9 +3,10 @@
 # Purpose: 
 
 # Variables
+$PROFILEPATH = "$PROFILE.CurrentUserAllHosts"
 $POSH_THEME = "https://github.com/ColoredBytes/Dotfiles/blob/4668e84e5f059172b6aecbd73a74ba6a37809f3d/assets/my.blue-owl.omp.json"
-$LOCATION = "C:\Users\rossj25\.config\oh-my-posh\themes"
-$COMMAND_VAR = '$env:POSH_THEMES_PATH="C:\Users\rossj25\.config\oh-my-posh\themes"'
+$LOCATION = "${env:USERPROFILE}\.config\oh-my-posh\themes\"
+$COMMAND_VAR = '$env:POSH_THEMES_PATH="${env:USERPROFILE}\.config\oh-my-posh\themes"'
 $INIT_COMMAND = 'oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\my.blue-owl.omp.json" | Invoke-Expression'
 
 # Create directory if it does not exist
@@ -20,19 +21,18 @@ winget install JanDeDobbeleer.OhMyPosh -s winget
 Invoke-WebRequest -Uri $POSH_THEME -OutFile "$LOCATION\my.blue-owl.omp.json"
 
 # Add $COMMAND_VAR and $INIT_COMMAND to $PROFILE.CurrentUserAllHosts
-$profilePath = $PROFILE.CurrentUserAllHosts
-$profileContent = if (Test-Path -Path $profilePath) { Get-Content -Path $profilePath } else { @() }
+$PROFILECONTENT = if (Test-Path -Path $PROFILEPATH) { Get-Content -Path $PROFILEPATH } else { @() }
 
-if (-not ($profileContent -contains $COMMAND_VAR)) {
-    Add-Content -Path $profilePath -Value $COMMAND_VAR
+if (-not ($PROFILECONTENT -contains $COMMAND_VAR)) {
+    Add-Content -Path $PROFILEPATH -Value $COMMAND_VAR
 }
 
-if (-not ($profileContent -contains $INIT_COMMAND)) {
-    Add-Content -Path $profilePath -Value $INIT_COMMAND
+if (-not ($PROFILECONTENT -contains $INIT_COMMAND)) {
+    Add-Content -Path $PROFILEPATH -Value $INIT_COMMAND
 }
 
 # Load the new profile
-. $profilePath
+. $PROFILEPATH
 
 
 
