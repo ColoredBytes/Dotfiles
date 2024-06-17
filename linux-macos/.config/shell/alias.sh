b273b0ac97907e5ded9387897e188c028dbd6687
Joshua Ross <josh@rossjm.dev>
2024-06-17 13:48:33 -0400

    Create alias.sh

---
+++ linux-macos/.config/shell/alias.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Author: Joshua Ross
+# Purpose: aliases to use in wsl.
+
+
+alias sc="grep ^[^#] $1" #(strips comments from file.) 
+alias vim="nvim" # set vim to open neovim instead.
+alias tree='tree --dirsfirst -F'
